85d5f84bb8563ce2064232107e1020d7ac62aa53
thepowderguy <powderthefirst@gmail.com>
2017-09-04 16:26:48 -0400

    gitignore

--- bot.py
+++ bot.py
@@ -8,13 +8,13 @@ token = tfile.read()
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------------')
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------------')
 
 @client.event
 async def on_message(message):
-    await client.send_message(message.channel, "Hello world")
+	await client.send_message(message.channel, "Hello world")
 
 client.run(token)
